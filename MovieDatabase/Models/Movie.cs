c7b3b25e0f75784d7d53046d792c12c7ab948d29
gabi <gabriella.marias@gmail.com>
2021-11-04 18:18:04 -0400

    First commit for Movie Database lab

---
+++ MovieDatabase/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieDatabase.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public double Runtime { get; set; }
+    }
+}
+
